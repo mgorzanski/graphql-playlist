24dc511d1868606397fd4c3c37cb421fe0a0bd00
Mateusz <gorzanski.mateusz@gmail.com>
2019-02-25 21:20:03 +0100

    finish course

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+mongoose.connect('mongodb://mateusz:zaq12wsx@ds063889.mlab.com:63889/gql-matigora');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
+});
